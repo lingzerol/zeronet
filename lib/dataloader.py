d3fa44ec5a27163fcdc5d48fe04a89c095239934
lingzerol <467264565@qq.com>
2020-04-04 14:18:29 +0800

    add multi domain process clas

--- lib/dataloader.py
+++ lib/dataloader.py
@@ -2,6 +2,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import os
 
+
 class CycleDataset(dset.VisionDataset):
     def __init__(self, root, loader=dset.folder.default_loader, extensions=('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp'),
                  transform=None, target_transform=None, is_valid_file=None):
@@ -22,9 +23,43 @@ class CycleDataset(dset.VisionDataset):
         sample = self.loader(path)
         if self.transform is not None:
             sample = self.transform(sample)
+        return sample
+
+    def __len__(self):
+        return len(self.samples)
+
+
+class MultiDomainDataset(dset.VisionDataset):
+    def __init__(self, root, loader=dset.folder.default_loader, extensions=('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp'),
+                 transform=None, target_transform=None, is_valid_file=None):
+        super(MultiDomainDataset, self).__init__(root, transform=transform,
+                                                 target_transform=target_transform)
+
+        samples = []
+        dirnum = 0
+        for i, dirname in enumerate(os.listdir(root)):
+            dirpath = os.path.join(root, dirname)
+            if not os.path.isdir(dirpath) and not os.path.islink(dirpath):
+                continue
+            if os.path.islink(dirpath):
+                dirpath = os.readlink(dirpath)
+            for filename in os.listdir(dirpath):
+                filepath = os.path.join(dirpath, filename)
+                if filename.lower().endswith(extensions):
+                    samples.append([filepath, dirnum])
+            dirnum += 1
+
+        self.loader = loader
+        self.samples = samples
+
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
         if self.target_transform is not None:
             target = self.target_transform(target)
-        return sample
+        return sample, target
 
     def __len__(self):
         return len(self.samples)
--- lib/datapool.py
+++ lib/datapool.py
@@ -31,3 +31,50 @@ class DataPool():
             idxes = np.random.randint(0, len(self.pool), batch)
             result = [self.pool[idx] for idx in idxes]
             return result
+
+
+class MutilDomainDataPool():
+    def __init__(self, pool_size, split=True):
+        self.pool_size = pool_size
+        self.split = split
+        self.pool = {}
+
+    def add(self, label, data):
+        if label not in self.pool.keys():
+            self.pool[label] = []
+        if self.split:
+            num = data.size(0)
+            for i in range(num):
+                if len(self.pool[label]) < self.pool_size:
+                    self.pool[label].append(data[i])
+                elif np.random.random() > 0.5:
+                    idx = np.random.randint(0, self.pool_size, 1)[0]
+                    self.pool[label][idx] = data[i]
+        else:
+            if len(self.pool[label]) < self.pool_size:
+                self.pool[label].append(data)
+            elif np.random.random() > 0.5:
+                idx = np.random.randint(0, self.pool_size, 1)[0]
+                self.pool[label][idx] = data
+
+    def query(self, label, batch=1):
+        if len(self.pool[label]) <= batch:
+            return self.pool[label].copy()
+        else:
+            idxes = np.random.randint(0, len(self.pool[label]), batch)
+            result = [self.pool[label][idx] for idx in idxes]
+            return result
+
+    def size(self, label=-1):
+        if label == -1:
+            num = 0
+            for i in self.pool:
+                num += len(self.pool[i])
+            return num
+        elif label not in self.pool:
+            return 0
+        else:
+            return len(self.pool[label])
+
+    def labels(self):
+        return list(self.pool.keys())
